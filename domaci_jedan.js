48d3c64ba4998d057dd25bd62ed2ab1fb2849f19
Sakie90 <sara_stevic@yahoo.com>
2017-09-07 23:22:02 +0200

    Add files via upload

---
+++ domaci_jedan.js
@@ -0,0 +1,15 @@
+
+
+
+function numberSum(N) {
+var total = 0;
+for(var i = 1; i <= N; i++){
+      total += i;
+    }
+    return total;
+}
+
+function run(){
+  val = document.getElementById("val").value;
+  document.getElementById("results").innerHTML=" suma je: "+numberSum(val)
+  }
